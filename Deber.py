f756657e5601996e4c2c82854c86d166b69b6910
saulmendezsalazar <shaulmendez@hotmail.com>
2018-07-11 21:48:38 -0500

    PrimerCodigo

---
+++ Deber.py
@@ -0,0 +1,20 @@
+
+
+print("Mi primer programa")
+
+print("SAUL MENDEZ  8 A")
+
+print ("Multiplicacion de dos numero.")
+
+a = float (input("Ingrese un valor para A"))
+b = float (input("Ingrese valor para B"))
+
+print ("La multiplicacion es :", a * b)
+
+
+print ("fin")
+
+
+
+
+
